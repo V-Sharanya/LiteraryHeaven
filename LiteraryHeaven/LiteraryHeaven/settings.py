6df0e5999a458c05459543a934678ee1f704425d
V-Sharanya <sharanyav.146@gmail.com>
2025-11-16 19:30:02 +0530

    added database to render

--- LiteraryHeaven/LiteraryHeaven/settings.py
+++ LiteraryHeaven/LiteraryHeaven/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
+
+import dj_database_url
 from dotenv import load_dotenv
 import os
 load_dotenv()
@@ -79,14 +81,11 @@ WSGI_APPLICATION = 'LiteraryHeaven.LiteraryHeaven.wsgi.application'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'mysql.connector.django',
-        'NAME': os.getenv('DB_NAME'),
-        'USER': os.getenv('DB_USER'),
-        'PASSWORD': os.getenv('DB_PASSWORD'),
-        'HOST': os.getenv('DB_HOST'),
-        'PORT': os.getenv('DB_PORT'),
-    }
+    'default': dj_database_url.config(
+            default=os.environ.get("DATABASE_URL"),
+            conn_max_age=600,
+            ssl_require=True
+        )
 }
 
 
