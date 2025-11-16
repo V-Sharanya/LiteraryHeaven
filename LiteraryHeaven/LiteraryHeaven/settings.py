0fa44adebc82368a9ef0aab11c47dd83f518a791
V-Sharanya <sharanyav.146@gmail.com>
2025-11-16 18:42:45 +0530

    secret key

--- LiteraryHeaven/LiteraryHeaven/settings.py
+++ LiteraryHeaven/LiteraryHeaven/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv("SECRET_KEY")
+SECRET_KEY = "django-insecure-a!8g^3zp#07zovqm!8r^asdv#zlm0@5ci3n9!0)n9155dd!2@y"
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
